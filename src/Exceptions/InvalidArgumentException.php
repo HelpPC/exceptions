ec8301e0ca232906e002155cbf6337b73d6621e8
Tomas Kulhanek <info@tirus.cz>
2019-01-28 23:34:06 +0100

    Init

---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: Tomas Kulhanek
+ * Email: info@tirus.cz
+ * Date: 2019-01-28
+ * Time: 23:22
+ */
+
+namespace HelpPC\Exceptions;
+
+
+class InvalidArgumentException extends \InvalidArgumentException
+{
+
+}
---
+++ src/Exceptions/InvalidStateException.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: Tomas Kulhanek
+ * Email: info@tirus.cz
+ * Date: 2019-01-28
+ * Time: 23:26
+ */
+
+namespace HelpPC\Exceptions;
+
+
+class InvalidStateException extends \RuntimeException
+{
+
+}
